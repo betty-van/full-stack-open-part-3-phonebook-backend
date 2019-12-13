da743d22867e411ec77b80059f1bdaf38be7211f
betty-van <bvan1412@gmail.com>
2019-12-12 20:32:19 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const http = require('http')
+
+let persons = [
+    {
+        name: "Arto Hellas",
+        number: "040-123456",
+        id: 1
+    },
+    {
+        name: "Ada Lovelace",
+        number: "39-33-532532",
+        id: 2
+    },
+    {
+        name: "Dan Abramov",
+        number: "12-43-234345",
+        id: 3
+    },
+    {
+        name: "Mary Poppendieck",
+        number: "39-23-642122",
+        id: 4
+    }
+]
+
+const app = http.createServer((request, response) => {
+  response.writeHead(200, { 'Content-Type': 'application/json' })
+  response.end(JSON.stringify(person))
+})
+
+const port = 3001
+app.listen(port)
+console.log(`Server running on port ${port}`)
