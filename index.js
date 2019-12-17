f1f841cd851cfe535b7b74b2f69dd69f34b6a4da
betty-van <bvan1412@gmail.com>
2019-12-16 19:26:38 -0800

    Added cors

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
+const cors = require('cors')
 
 app.use(bodyParser.json())
+app.use(cors())
 
 // Used to log HTTP requests to the console
 app.use(morgan((tokens, req, res) => {
